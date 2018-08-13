da8ad8178f6ec503ad3a10f33c65d93a6fe18db8
Steven Crouse <sdcrouse91@gmail.com>
2018-08-13 03:06:32 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
 def my_each(array) # put argument(s) here
   # code here
-  
+  yield array[0]
   #if block_given? # This if/else may not be necessary for this lab.
     #counter = 0 
     #while counter < array.size 
-      yield array[counter]
+      #yield array[counter]
       #counter += 1
     #end
     
