541ec54a952b3c1bb1bafe376a670953a83a6c2d
Steven Crouse <sdcrouse91@gmail.com>
2018-08-13 03:07:48 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,15 @@
 def my_each(array) # put argument(s) here
   # code here
-  yield array[0]
-  #if block_given? # This if/else may not be necessary for this lab.
-    #counter = 0 
-    #while counter < array.size 
-      #yield array[counter]
-      #counter += 1
-    #end
+  
+  if block_given? # This if/else may not be necessary for this lab.
+    counter = 0 
+    while counter < array.size 
+      yield array[counter]
+      counter += 1
+    end
     
-    #array
-  #else
-    #"You need to pass a block!"
-  #end
+    array
+  else
+    "You need to pass a block!"
+  end
 end
